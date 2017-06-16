425394f479878bc14ef21a21204a52e263489d0a
codingconcepts <bassrob@gmail.com>
2017-06-16 11:13:00 +0100

    Added doc comment

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package shape allows structs to be populated directly from environment
+// variables.  Simply create a struct tag called "env", and optionally
+// provided a "required" tag to determine whether an error should be
+// returned in the even of missing environment configuration and call the
+// shape.Env function.
+package shape
