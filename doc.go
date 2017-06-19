11353695c86f1b72b137478177ca7a9cae11d384
codingconcepts <bassrob@gmail.com>
2017-06-19 07:47:43 +0100

    Added more info to doc comment

--- doc.go
+++ doc.go
@@ -1,5 +1,11 @@
 // Package shape allows struct fields to be populated directly from environment
-// variables.  Simply create a struct tag called "env", and optionally provided
-// a "required" tag to determine whether an error should be returned in the even
-// of missing environment configuration and call the shape.Env function.
+// variables.
+//
+// To use, create a struct tag called "env" and call shape.Env, passing a pointer
+// to the struct you wish to populate.  You can optionally, provide a "required"
+// tag to determine whether an error should be returned in the event of missing
+// environment configuration.
+//
+// Like the encoding/* packages, shape.Env will return an error if a non-pointer
+// type is provided.
 package shape
